4f50b9ae896f00544de9d3310f224189d99006b6
Zanella Matteo <79336755+Zanzibarr@users.noreply.github.com>
2022-05-27 17:12:06 +0200

    Little Changes

--- src/main/java/com/broject/eutrustlocal/Query/Query.java
+++ src/main/java/com/broject/eutrustlocal/Query/Query.java
@@ -128,7 +128,7 @@ public class Query {
             String[] parameterArray = {_parameter};
             filters.get(filterIndex).addParameters(parameterArray);
             if (filterIndex == 0 && !fullCountriesArchive && !countriesArchive.contains(_parameter)) {
-                System.out.println("Adding parameter: "+_parameter);                                                                        /*------------------------------------------------------------------*/
+                System.out.println("Adding " + _parameter + " : " + addedCountries);                                                        /*------------------------------------------------------------------*/
                 countriesArchive.add(_parameter);
                 addedCountries.add(_parameter);
                 newRequestNeeded = true;
