2736b9b0ec41173459bc5f9ca1a685be2759b8fc
RickSrick <79211132+RickSrick@users.noreply.github.com>
2022-06-04 16:53:25 +0200

    Last views added. Founded some bugs on navigation.

---
+++ src/main/java/com/broject/eutrustlocal/App.java
@@ -0,0 +1,15 @@
+package com.broject.eutrustlocal;
+
+/**
+ * @author Riccardo Modolo<br>
+ *
+ * this class is only for build .jar
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+
+}
